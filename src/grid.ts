3d2bd7151e895ffa3097131abc5ae176aedfdebe
Smoqu <36056008+Smoqu@users.noreply.github.com>
2021-02-21 12:46:06 +0100

    Fixing types

--- src/grid.ts
+++ src/grid.ts
@@ -1,72 +1,73 @@
-
 class Square {
-  constructor(scale, x, y, color) {
-    this.scale = scale
-    
-    this.coords = {x, y}
+  scale: number;
+  coords: { x: number; y: number };
+  color: string;
+  constructor(scale: number, x: number, y: number, color: string) {
+    this.scale = scale;
+
+    this.coords = { x, y };
     this.color = color;
     // this.translate()
   }
-  
-//   translate() {
-//     const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-    
-//     this.coords.x = letters[this.column]
-//     this.coords.y = this.row - 1
-    
-//     return {x: letters[this.column], y: this.row - 1}
-//   }
-  
+
+  //   translate() {
+  //     const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+  //     this.coords.x = letters[this.column]
+  //     this.coords.y = this.row - 1
+
+  //     return {x: letters[this.column], y: this.row - 1}
+  //   }
+
   show() {
-    if (this.color === "blue")
-      fill(0, 150, 200)
-    else fill(27, 27, 27)
-    rect(this.coords.x * this.scale, this.coords.y* this.scale, this.scale)
-    
+    if (this.color === "blue") fill(0, 150, 200);
+    else fill(27, 27, 27);
+    rect(
+      this.coords.x * this.scale,
+      this.coords.y * this.scale,
+      this.scale,
+      this.scale
+    );
   }
 }
 
 class Grid {
-  constructor(whichIsIt) {
-    
-    this.grid = new Array(10)
-    this.whichIsIt = whichIsIt
-    
-    for( let i = 0; i < 10; i++) {
-      this.grid[i] = new Array(10)
+  grid: Array<Array<Square>>;
+  whichIsIt: string;
+  constructor(whichIsIt: string) {
+    this.grid = new Array(10);
+    this.whichIsIt = whichIsIt;
+
+    for (let i = 0; i < 10; i++) {
+      this.grid[i] = new Array(10);
     }
-    
-    const scale = size / 20
-    
-    let color;
-    
-    if (this.whichIsIt === "mine")
-      color = "blue"
-    
+
+    const scale = size / 20;
+
+    let color: string;
+
+    if (this.whichIsIt === "mine") color = "blue";
+    else color = "grey";
+
     for (let x = 0; x < 10; x++) {
-      for(let y = 0; y < 10; y++) {
-        this.grid[x][y] = new Square(scale, x, y, color)
-        console.log(x, y)
+      for (let y = 0; y < 10; y++) {
+        this.grid[x][y] = new Square(scale, x, y, color);
+        console.log(x, y);
       }
     }
   }
-  
+
   show() {
-    push()
-    
-    if (this.whichIsIt === "mine")
-        translate(width / 4, height / 2)
-    else
-      translate(width / 4, 0)
-      
+    push();
+
+    if (this.whichIsIt === "mine") translate(width / 4, height / 2);
+    else translate(width / 4, 0);
+
     for (let i = 0; i < this.grid.length; i++) {
       for (let j = 0; j < this.grid[i].length; j++) {
-        
-        this.grid[i][j].show()
+        this.grid[i][j].show();
       }
     }
-    pop()
+    pop();
   }
-  
-  
 }
